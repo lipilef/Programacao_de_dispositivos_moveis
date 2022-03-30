2ce2d9cb8e5c4b0d54cc9a60050593dbc7f6ec2c
Felipe Alves <lipilef2nd@gmail.com>
2022-03-29 22:39:46 -0300

    Add files via upload

--- Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/IMC.kt
+++ Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/IMC.kt
@@ -2,36 +2,42 @@ package com.example.imc
 
 import android.os.Parcel
 import android.os.Parcelable
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
 
 class IMC(var nome: String?, var peso: Float, var altura: Float, var imc: Float) :Parcelable {
 
 
     constructor(parcel: Parcel) : this(
         parcel.readString(),
-        parcel.readFloat(),
-        parcel.readFloat(),
-        parcel.readFloat()
+        parcel.readFloat(), //imc
+        parcel.readFloat(), //peso
+        parcel.readFloat() //altura
     ) {
     }
 
-    constructor(name: String, peso: Float, altura: Float) : this(name,peso,altura,0.0f)
+    constructor(name: String, peso: Float, altura: Float) : this(name,peso,altura,0.2f)
 
     fun calcular() : String{
 
-        val alt =altura/100
-            val calc = peso / (alt*alt)
-            val msg =when (calc){
+                val alt = altura/100
+                val calc = peso / (alt*alt)
+                val msg = when (calc){
                 in 0 ..16  -> "Magreza grave"
-                in 16 ..17 ->"Magreza moderada"
-                in 17 ..19 ->"Magreza leve"
-                in 19 ..25 ->"Saúdavel"
-                in 25 ..30 ->"Sobrepeso"
-                in 30 ..35 ->"Nível I de proteção contra o frio"
-                in 35 ..40 ->"Nível II de proteção contra o frio"
-                else -> "Obsedidade Morbida."
+                in 16 ..17 -> "Magreza moderada"
+                in 17 ..19 -> "Magreza leve"
+                in 19 ..25 -> "Saúdavel"
+                in 25 ..30 -> "Sobrepeso"
+                in 30 ..35 -> "Nível I de proteção contra o frio"
+                in 35 ..40 -> "Nível II de proteção contra o frio"
+                else -> "Nível III ou mais."
             }
 
-        imc = calc
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.CEILING
+
+        imc = df.format(calc).toFloat()
         return msg
     }
 
--- Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/ResultadoActivity.kt
+++ Celula_03/Bloco_01/IMC/app/src/main/java/com/example/imc/ResultadoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.imc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -7,15 +8,18 @@ import kotlinx.android.synthetic.main.activity_resultado2.*
 
 class ResultadoActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado2)
 
         val imc = intent.getParcelableExtra<IMC>("value")
+
         titleDensidadeCorporal.text = imc.nome.toString()
         textViewHeaderDensidadeCorporal.text = imc.calcular()
-        textViewResultadoDensidadeCorporal.text = imc.imc.toString()
-
+        textViewResultadoDensidadeCorporal.text = "Seu IMC "+imc.imc.toString()
+        textViewPeso.text = "Seu Peso "+imc.peso.toString()+" kg"
+        textViewAltura.text = "Sua Altura "+imc.altura.toString()
 
     }
 }
